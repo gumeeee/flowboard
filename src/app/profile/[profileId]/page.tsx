2479d5ae5264376aaa712ea8741bd822bf745b36
gumeeee <gaxco123@gmail.com>
2025-02-03 22:51:48 -0300

    feat: add ProfileView component to view public profile users, and add suppressHydrationWarning in layout.tsx

--- src/app/profile/[profileId]/page.tsx
+++ src/app/profile/[profileId]/page.tsx
@@ -1,7 +1,40 @@
+import { users } from "@/utils/users";
+import { notFound } from "next/navigation";
 import React from "react";
+import { ProfileView } from "./ProfileView";
+import { createClient } from "@/utils/supabase/server";
 
-const PublicProfilePage = () => {
-  return <div>PublicProfilePage</div>;
+async function getUser(profileId: string): Promise<IUser> {
+  try {
+    const user = await users.getUser(profileId);
+    if (!user) throw new Error("User not found");
+    return user;
+  } catch (error) {
+    console.error(error);
+    notFound();
+  }
+}
+
+const PublicProfilePage = async ({
+  params,
+}: {
+  params: Promise<{ profileId: string }>;
+}) => {
+  const { profileId } = await params;
+
+  const user = await getUser(profileId);
+
+  const supabase = await createClient();
+  const {
+    data: { user: currentUser },
+  } = await supabase.auth.getUser();
+  const isOwnProfile = currentUser?.id === user.id;
+
+  return (
+    <div className="my-16">
+      <ProfileView user={user} isOwnProfile={isOwnProfile} />
+    </div>
+  );
 };
 
 export default PublicProfilePage;
